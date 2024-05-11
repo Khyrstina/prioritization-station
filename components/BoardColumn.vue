23c1d38947f5886cdb522ead6041267a5583ab4b
Khyrstina Smith <Khyrstinasmith@gmail.com>
2024-05-11 19:05:04 -0400

    move columns to their own component

---
+++ components/BoardColumn.vue
@@ -0,0 +1,54 @@
+<script setup>
+import {useBoardStore} from "../stores/boardStore.ts";
+const boardStore = useBoardStore()
+
+
+defineProps({
+  column: {
+    type: Object,
+    required: true,
+  },
+  columnIndex: {
+    type: Number,
+    required: true,
+  },
+})
+const editNameState = ref(false)
+
+function deleteColumn(columnIndex) {
+  boardStore.deleteColumn(columnIndex)
+}
+</script>
+
+<template>
+  <UContainer
+      class="column">
+    <div class="column-header mb-4">
+      <div>
+        <UInput v-if="editNameState" type="text" v-model="column.name">
+        </UInput>
+        <h2 v-else class="mb-4">{{ column.name }}</h2>
+      </div>
+      <div>
+        <UButton
+            icon="i-heroicons-pencil-square"
+            color="purple"
+            class="mr-2"
+            @click="editNameState = !editNameState"/>
+        <UButton
+            icon="i-heroicons-trash"
+            color="red"
+            @click="deleteColumn(columnIndex)"/>
+      </div>
+    </div>
+    <ul>
+      <li v-for="task in column.tasks"
+          :key="task.id">
+        <UCard class="mb-4">
+          <h5>{{ task.name }}</h5>
+          <p>{{ task.description }}</p>
+        </UCard>
+      </li>
+    </ul>
+  </UContainer>
+</template>
