06b743187091b2babf510c5924f537ee1feed422
Khyrstina Smith <Khyrstinasmith@gmail.com>
2024-05-11 19:05:25 -0400

    refactor index.vue so for moving columns to their own component

--- pages/index.vue
+++ pages/index.vue
@@ -4,51 +4,21 @@ import { useStorage} from "@vueuse/core";
 
 const boardStore = useBoardStore()
 const newColumnName = ref('')
-const editNameState = ref(false)
 
 function addColumn() {
   boardStore.addColumn(newColumnName.value)
   newColumnName.value = ''
 }
-function deleteColumn(columnIndex) {
-  boardStore.deleteColumn(columnIndex)
-}
 </script>
 <template>
   <div class="board-wrapper">
   <main class="board">
-    <UContainer
+    <BoardColumn
          v-for="(column, columnIndex) in boardStore.board.columns"
-         :key="column.name"
-         class="column">
-      <div class="column-header mb-4">
-        <div>
-        <UInput v-if="editNameState" type="text" v-model="column.name">
-        </UInput>
-          <h2 v-else class="mb-4">{{ column.name }}</h2>
-        </div>
-        <div>
-          <UButton
-          icon="i-heroicons-pencil-square"
-          color="purple"
-          class="mr-2"
-          @click="editNameState = !editNameState"/>
-          <UButton
-              icon="i-heroicons-trash"
-              color="red"
-              @click="deleteColumn(columnIndex)"/>
-        </div>
-      </div>
-      <ul>
-        <li v-for="task in column.tasks"
-        :key="task.id">
-          <UCard class="mb-4">
-          <h5>{{ task.name }}</h5>
-          <p>{{ task.description }}</p>
-          </UCard>
-        </li>
-      </ul>
-    </UContainer>
+         :key="column.id"
+         :column="column"
+          :columnIndex="columnIndex">
+    </BoardColumn>
     <UContainer class="column">
       <UInput type="text"
               v-model="newColumnName"
