e8dc39c98164f6c05c817b808ffc1532162ed8d1
Khyrstina Smith <Khyrstinasmith@gmail.com>
2024-05-11 21:22:15 -0400

    Add handling for moving columns

--- components/BoardColumn.vue
+++ components/BoardColumn.vue
@@ -32,30 +32,50 @@ function goToTask(taskId) {
   router.push(`/tasks/${taskId}`)
 }
 
+function pickupColumn(event, fromColumnIndex) {
+  event.dataTransfer.effectAllowed = 'move'
+  event.dataTransfer.dropEffect = 'move'
+  event.dataTransfer.setData('type', 'column')
+  event.dataTransfer.setData('from-column-index', fromColumnIndex)
+}
+
 function pickUpTask(event, { fromColumnIndex, fromTaskIndex }) {
   event.dataTransfer.effectAllowed = 'move'
   event.dataTransfer.dropEffect = 'move'
+  event.dataTransfer.setData('type', 'task')
   event.dataTransfer.setData('from-column-index', fromColumnIndex)
   event.dataTransfer.setData('from-task-index', fromTaskIndex)
 }
 
-function dropTask(event, toColumnIndex) {
+function dropItem(event, toColumnIndex) {
+  const type = event.dataTransfer.getData('type')
   const fromColumnIndex = event.dataTransfer.getData('from-column-index')
-  const fromTaskIndex = event.dataTransfer.getData('from-task-index')
 
-  boardStore.moveTask({
-    taskIndex: fromTaskIndex,
-    fromColumnIndex,
-    toColumnIndex,
-  })
+  if( type === 'task') {
+    const fromTaskIndex = event.dataTransfer.getData('from-task-index')
+
+    boardStore.moveTask({
+      taskIndex: fromTaskIndex,
+      fromColumnIndex,
+      toColumnIndex,
+    })
+  } else if (type === 'column') {
+    boardStore.moveColumn({
+      fromColumnIndex,
+      toColumnIndex,
+    })
+  }
+
 }
 </script>
 
 <template>
   <UContainer class="column"
+              draggable="true"
+              @dragstart.self="pickupColumn($event, columnIndex)"
               @dragenter.prevent
               @dragover.prevent
-              @drop.stop="dropTask($event, columnIndex)">
+              @drop.stop="dropItem($event, columnIndex)">
     <div class="column-header mb-4">
       <div>
         <UInput v-if="editNameState" type="text" v-model="column.name">
