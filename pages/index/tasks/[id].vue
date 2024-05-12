17033805a60571bed7d3e8eee0b702f75371d41b
Khyrstina Smith <Khyrstinasmith@gmail.com>
2024-05-11 20:28:50 -0400

    add delete option for tasks

--- pages/index/tasks/[id].vue
+++ pages/index/tasks/[id].vue
@@ -4,17 +4,34 @@ import { useBoardStore} from "~/stores/boardStore.ts";
 
 const boardStore = useBoardStore()
 const route = useRoute()
+const router = useRouter()
 
 const task = computed(() => {
   return boardStore.getTask(route.params.id);
 })
+
+function deleteTask() {
+  boardStore.deleteTask(route.params.id)
+  router.push('/')
+}
 </script>
 
 <template>
   <div class="task-wrapper">
-    <div class="task-view">
-      <h1>{{ task.name }}</h1>
-      <p>{{ task.description }}</p>
+    <div v-if="task" class="task-view">
+      <uFormGroup label="Name">
+        <UInput type="text" v-model="task.name" class="w-full mb-4"/>
+      </uFormGroup>
+      <uFormGroup label="Description" class="w-full mb-4">
+        <UTextarea v-model="task.description" />
+      </uFormGroup>
+      <UButton
+          icon="i-heroicons-trash"
+          color="red"
+          @click="deleteTask">Delete Task</UButton>
+    </div>
+    <div v-else>
+      <p>Task not found</p>
     </div>
   </div>
 </template>
